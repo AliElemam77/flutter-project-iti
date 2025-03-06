9000db85c50ca03a627559f6c1a4e2760ee2b8d6
Ali Elemam <alielemam515@gmail.com>
2025-03-06 03:26:42 +0200

    Flutter Task one

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,19 +8,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.redAccent,
-        onPressed: () {
-          setState(() {
-            _counter++;
-          });
-        },
-        child: const Icon(Icons.add, color: Colors.white),
-      ),
       appBar: AppBar(
         backgroundColor: Colors.redAccent,
         title: const Text(
@@ -34,27 +24,67 @@ class _HomeState extends State<Home> {
         actions: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text(
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-              "$_counter",
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.settings, color: Colors.white),
             ),
           ),
         ],
       ),
-      body: const Center(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [Text('Look at the top right corner' ,style: TextStyle(
-              color: Colors.redAccent,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),)],
-          ),
+      body: Center(
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+
+              child: Container(color: Colors.red, child: Column()),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.green,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              height: 100,
+                              color: Colors.blueGrey,
+                              child: Center(child: Text('one')),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 100,
+                              color: Colors.redAccent,
+                              child: Center(child: Text('two')),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 100,
+                              color: Colors.amberAccent,
+                              child: Center(child: Text('three')),
+                            ),
+                          ),
+                         ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+
+              child: Container(color: Colors.blue, child: Column()),
+            ),
+          ],
         ),
       ),
     );
